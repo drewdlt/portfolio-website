a037f452a778a42daabf7fea5ec71b65dc0e684a
Andrew De La Torre <drewdlt@yahoo.com>
2026-03-07 22:58:45 -0800

    Change profile image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,30 @@
-import { useState } from "react";
-import "./Home.css";
-import { NavLink } from "react-router";
+import { useState } from 'react';
+import './Home.css';
 
 export function Home() {
-  const [showSidebar, setShowSidebar] = useState(false);
+	const [showSidebar, setShowSidebar] = useState(false);
 
-  return (
-    <>
-      <div className="items-center flex flex-col gap-3">
-        <img src="PortfolioImage.WebP" alt="" className="profile-image" />
-        <div className="flex flex-col gap-2 ">
-          <h1 className="text-xl font-bold text-center">
-            Hi, I'm Andrew De La Torre
-          </h1>
-          <h3 className="text-lg font-semibold text-center">
-            Full-Stack Developer
-          </h3>
-          <p className="mx-5">
-            Experience building front-end and back-end applications with an
-            interest in creating clean and user-friendly interfaces.
-          </p>
-          <p className="text-center">HTML • CSS • JavaScript • Java</p>
-          <p className="text-center">React • Spring Boot • TailwindCSS</p>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='items-center flex flex-col gap-3 my-4'>
+				<div className='profile-image-container'>
+					<div className='profile-image'></div>
+				</div>
+				<div className='flex flex-col gap-2 '>
+					<h1 className='text-xl font-bold text-center'>
+						Hi, I'm Andrew De La Torre
+					</h1>
+					<h3 className='text-lg font-semibold text-center'>
+						Full-Stack Developer
+					</h3>
+					<p className='mx-5'>
+						Experience building front-end and back-end applications with an
+						interest in creating clean and user-friendly interfaces.
+					</p>
+					<p className='text-center'>HTML • CSS • JavaScript • Java</p>
+					<p className='text-center'>React • Spring Boot • TailwindCSS</p>
+				</div>
+			</div>
+		</>
+	);
 }
